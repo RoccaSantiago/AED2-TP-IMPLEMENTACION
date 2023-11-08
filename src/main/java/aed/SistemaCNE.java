b18218b32fc516347378755ff033b766c539795b
Gonzalo Lopez <gonzalo.esloga@gmail.com>
2023-11-07 22:48:52 -0300

    Actualizacion archivo main agregue funciones 1,2,3,4 revisar

--- src/main/java/aed/SistemaCNE.java
+++ src/main/java/aed/SistemaCNE.java
@@ -1,6 +1,27 @@
 package aed;
+
+import Clases.Nodo;
+import Clases.array;
+
 public class SistemaCNE {
     // Completar atributos privados
+
+    private String[] nombresPartidos;   
+    private String[] nombresDistritos;
+    private int[] diputadosDeDistrito; // Cambie el nombre de diputadosEnDisputa a diputadosDeDistrito @Gonza
+    private int[] rangoMesasDistritos; // es distritosDeMesa por ahora
+
+    // private AVL    DistrictosDeMesa;
+    // private int[] DistrictosDeMesa;
+
+    // private int[] bancasDeDistricto; PUede que sea el mismo que diputadosEnDisputa
+    private int[] votosPresidenciales;
+    private int[][] votosDiputados;
+
+    // private heap hayBallotage; Despues lo ponemos cuando el heap este listo
+
+    private int[] hayBallotage; // 3 elementos, [0] Maximo, [1] Segundo Maximo, [2] votos totales, tambien pude ser un vector @Gonza
+    
 
     public class VotosPartido{
         private int presidente;
@@ -11,23 +32,89 @@ public class SistemaCNE {
     }
 
     public SistemaCNE(String[] nombresDistritos, int[] diputadosPorDistrito, String[] nombresPartidos, int[] ultimasMesasDistritos) {
-        throw new UnsupportedOperationException("No implementada aun");
+        
+        //inicializacion de variables
+        
+        this.nombresPartidos = new String[nombresPartidos.length];
+        this.nombresDistritos = new String[nombresDistritos.length];
+        this.diputadosDeDistrito = new int[diputadosPorDistrito.length];
+        this.rangoMesasDistritos = new int[ultimasMesasDistritos.length];
+
+        // input de nombresPartidos
+
+        for (int i = 0; i<nombresPartidos.length; i++){
+            this.nombresPartidos[i] = nombresPartidos[i];
+        }
+
+        // input de nombresDistrictos, diputadosEnDisputa y rangoMesasDistritos
+
+        for (int i = 0; i<nombresDistritos.length; i++){
+            this.nombresDistritos[i] = nombresDistritos[i];
+            this.diputadosDeDistrito[i] = diputadosPorDistrito[i];
+            this.rangoMesasDistritos[i] = ultimasMesasDistritos[i] - 1;
+
+
+            // Pensado con rangoMesasDistritos como matriz y no como array @Gonza
+            //if (i > 0){
+            //    this.rangoMesasDistritos[i][0] = ultimasMesasDistritos[i-1];
+            //    this.rangoMesasDistritos[i][1] = ultimasMesasDistritos[i];
+            //}
+            //else {
+            //    this.rangoMesasDistritos[0][0] = 0;
+            //    this.rangoMesasDistritos[0][1] = ultimasMesasDistritos[0];
+            //}
+        }
+
+
     }
 
     public String nombrePartido(int idPartido) {
-        throw new UnsupportedOperationException("No implementada aun");
+        return nombresPartidos[idPartido];
     }
 
     public String nombreDistrito(int idDistrito) {
-        throw new UnsupportedOperationException("No implementada aun");
+        return nombresDistritos[idDistrito];
     }
 
     public int diputadosEnDisputa(int idDistrito) {
-        throw new UnsupportedOperationException("No implementada aun");
+        return diputadosDeDistrito[idDistrito];
     }
 
     public String distritoDeMesa(int idMesa) {
-        throw new UnsupportedOperationException("No implementada aun");
+        int n = nombresDistritos.length/2;
+        n = this.bbDistritodeMesa(idMesa, n);
+        return nombreDistrito(n);
+
+    }
+
+    private int bbDistritodeMesa(int idMesa, int n){ // Mi intento de busqueda binaria, no se si hace division entera el / porfa diganme si no lo hace @Gonza
+        if (rangoMesasDistritos[n] > idMesa){
+
+            if (n == 0){
+                return n;
+            }
+            else if (rangoMesasDistritos[n-1] < idMesa){
+                return n;
+            }
+            else {
+            n = n - n / 2; // me complique con este facor, lo demas funciona bien
+            return bbDistritodeMesa(idMesa, n);
+            }
+        }
+        else if (rangoMesasDistritos[n] < idMesa){
+
+            if (n == nombresDistritos.length - 1){
+                return n;
+            }
+            else if (rangoMesasDistritos[n+1] > idMesa){
+                return (n+1);
+            }
+            else {
+            n = n + n/2; // este factor tambien
+            return bbDistritodeMesa(idMesa, n);
+            }
+        }
+        return n;
     }
 
     public void registrarMesa(int idMesa, VotosPartido[] actaMesa) {
@@ -46,8 +133,22 @@ public class SistemaCNE {
         throw new UnsupportedOperationException("No implementada aun");
     }
 
-    public boolean hayBallotage(){
-        throw new UnsupportedOperationException("No implementada aun");
+    public boolean hayBallotage(){ // Si quedamos con el heap despues se cambian las asignaciones solamente @Gonza
+        int votosTotales = hayBallotage[2];
+        int max1 = hayBallotage[0];
+        int max2 = hayBallotage[1];
+        float porcentajeMax1 = max1 / votosTotales * 100;
+        float porcentajeMax2 = max2 / votosTotales * 100;
+
+        if (porcentajeMax1 > 45){
+            return false;
+        }
+        else if (porcentajeMax1 > 40 && (porcentajeMax1 - porcentajeMax2) > 10){
+            return false;
+        }
+        else {
+            return true;
+        }
     }
 }
 
